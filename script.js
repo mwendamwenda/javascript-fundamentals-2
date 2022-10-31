7018c1485280e627399b273913e2b29058b36ca2
mwendamwenda <benmwenda83@gmail.com>
2022-10-31 08:29:33 +0300

    finacommit

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+//strct mode will create visisble arrors on our dev tools
+// strict mode will also show us reserved js word
+//strict mode will make us 
+// eg
+
+const private = 'kenyan boy';//when logged to the console this will show error because private is a revrseved word
